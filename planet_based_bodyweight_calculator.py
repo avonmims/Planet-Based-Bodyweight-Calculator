e7c347e221226120d96efc7c8f30aa7a39630b71
avonmims <avonmims@gmail.com>
2025-06-10 15:37:48 -0400

    in the proccess of adding moons, potentially changing output into a dataframe for better readability

--- planet_based_bodyweight_calculator.py
+++ planet_based_bodyweight_calculator.py
@@ -5,13 +5,33 @@ x = int(input("Enter weight (number only): "))
 def weight_planet():
 
     mercury = x * 0.378
+    
     venus = x * 0.903
+    
     earth = x * 1.0
+    moon = x * 0.165
+    
     mars = x * 0.380
+    
     jupiter = x * 2.640
+    io = x * 0.183
+    europe = x * 0.134
+    ganymede = x * 0.146
+    callisto = x * 0.1259
+    
     saturn = x * 1.140
+    titan = x * 0.138
+    iapetus = x * 0.022
+    mimas = x * 0.006
+    enceladus = x * 0.011
+    tethys = x * 0.015
+    dione = x * 0.024
+    rhea = x * 0.027
+    
     uranus = x * 0.918
+    
     neptune = x * 1.15
+    
     pluto = x * .063
 
     print(f'Your weight on each planet:''\n',
@@ -28,4 +48,5 @@ def weight_planet():
     
 weight_planet()
 
-# This script calculates the weight of a person on different planets using Eath's gravity as a reference.
+# This script calculates the weight of a person on different celestial
+# bodies in our solar system, using Eath's gravity as a reference.
